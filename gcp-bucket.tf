04200e8deea3be89766012b735bad966740f8106
ariqmbrilian <61818849+ariqmbrilian@users.noreply.github.com>
2022-01-27 21:51:02 +0700

    tf

--- gcp-bucket.tf
+++ gcp-bucket.tf
@@ -1,6 +1,6 @@
-resource "google_storage_bucket" "static-site" {
-  name          = "image-store.com"
-  location      = "EU"
+resource "google_storage_bucket" "simple_bucket" {
+  name          = "simple-bucket"
+  location      = var.region
   force_destroy = true
 
   uniform_bucket_level_access = true
